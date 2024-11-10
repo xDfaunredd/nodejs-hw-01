742b0289493a8a1181a1e503f8d072cc6cea3fff
xd.faunredd <andrew.furevych@gmail.com>
2024-11-10 17:17:02 +0200

    fix removeLastContact

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -4,9 +4,10 @@ import { writeContacts } from '../utils/writeContacts';
 export const removeLastContact = async () => {
   try {
     const data = await readContacts();
-    const newArray = data.pop();
-
-    await writeContacts(newArray);
+    if (data.length > 0) {
+      const newArray = data.pop();
+      await writeContacts(newArray);
+    }
   } catch (error) {
     console.log(error.message);
   }
